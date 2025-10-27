b837ca8c2a4f527288149d9224b4659e1ac5ffde
Douglas Jasper <doug.jasper@trios.com>
2025-10-27 08:12:32 -0400

    Initial Commit

---
+++ UserAgeApp/UserAgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAgeAppApp.swift
+//  UserAgeApp
+//
+//  Created by Douglas Jasper on 2025-10-27.
+//
+
+import SwiftUI
+
+@main
+struct UserAgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UserAgeAppTests/UserAgeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  UserAgeAppTests.swift
+//  UserAgeAppTests
+//
+//  Created by Douglas Jasper on 2025-10-27.
+//
+
+import Testing
+@testable import UserAgeApp
+
+struct UserAgeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
